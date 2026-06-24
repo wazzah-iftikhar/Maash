69c85c8e6469924fdc917c380d00e6ed0360004f
Wazzah <wazzah@marham.pk>
2026-06-24 15:56:01 +0500

    feat: add gold progress bar for page navigation

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,13 @@
 import type { AppProps } from 'next/app'
 import Layout from '@/components/Layout'
+import { AppProgressBar as ProgressBar } from 'next-nprogress-bar'
 import '@/styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Component {...pageProps} />
+      <ProgressBar height="3px" color="#C9A84C" options={{ showSpinner: false }} shallowRouting />
     </Layout>
   )
 }
